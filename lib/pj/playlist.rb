e08ee1fc4a691730f72b676ec54dad4c61170578
Yossef Mendelssohn <ymendel@pobox.com>
2008-10-07 18:49:53 -0500

    Giving tracks to a playlist.

--- lib/pj/playlist.rb
+++ lib/pj/playlist.rb
@@ -1,5 +1,9 @@
 module PJ
   class Playlist
-    attr_accessor :name
+    attr_accessor :name, :tracks
+    
+    def initialize
+      @tracks = []
+    end
   end
 end
--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -8,4 +8,14 @@ describe PJ::Playlist do
   it 'should have a name' do
     @playlist.should respond_to(:name)
   end
+  
+  it 'should have tracks' do
+    @playlist.should respond_to(:tracks)
+  end
+  
+  describe 'when initialized' do
+    it 'should have an empty track list' do
+      PJ::Playlist.new.tracks.should == []
+    end
+  end
 end
